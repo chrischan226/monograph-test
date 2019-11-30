082009858df104364b67c0808d26beb547b3a0c5
Chris Chan <chrischan226@gmail.com>
2019-11-30 13:34:25 -0800

    Add responsive mobile styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import Header from './Header.js';
 import Form from './Form.js';
 import Preview from './Preview.js';
 
 function App() {
-  const [{ feedback, party, emoji, storage }, setState] = 
-    useState({ feedback: '', party: 2, emoji: 4, storage: ['😡', '🙁', '😐', '😊', '😍'] });
-
+  const [{ feedback, party, emoji, storage, width }, setState] = 
+    useState({ feedback: '', party: 2, emoji: 4, storage: ['😡', '🙁', '😐', '😊', '😍'], width: window.innerWidth });
+  
   const update = ({target: { name, value }}) => {
     setState(prevState => ({ ...prevState, [name]: value }));
   }
 
+  const isMobile = width <= 900;
+
+  const handleWindowresize = useCallback(() => {
+    update({'target' : {'name': 'width', 'value':  window.innerWidth }});
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowresize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowresize);
+    };
+  }, [handleWindowresize]);
+
+
   return (
     <div className="app">
-      <Header />
-      <main className="app-main">
-        <section className="app-left">
-          <Form update = {update} feedback = {feedback} party = {party} emoji = {emoji} storage = {storage} />
-        </section>
-        <section className="app-right">
-          <Preview feedback = {feedback} partySize = {party} emoji = {emoji} storage = {storage}/>
-        </section>
-      </main>
+      {isMobile ?
+      <div>
+        <Header />
+        <main className="app-main">
+          <section className="app-top">
+            <Preview feedback = {feedback} partySize = {party} emoji = {emoji} storage = {storage}/>
+          </section>
+          <section className = "app-bottom">
+            <Form update = {update} feedback = {feedback} party = {party} emoji = {emoji} storage = {storage} />
+          </section>
+        </main>
+      </div>
+      :
+      <div>
+        <Header />
+        <main className="app-main">
+          <section className="app-left">
+            <Form update = {update} feedback = {feedback} party = {party} emoji = {emoji} storage = {storage} />
+          </section>
+          <section className="app-right">
+            <Preview feedback = {feedback} partySize = {party} emoji = {emoji} storage = {storage}/>
+          </section>
+        </main>
+      </div>
+    }
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import logo from './logo.png';
 import './Header.css';
 
 function Header() {
+  
   return (
     <header className="header">
       <div className="header-logo"></div>
